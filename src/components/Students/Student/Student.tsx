29053c326ab007471407c613447474127e2f5dab
ekatkaoff <ekaterinakazane@gmail.com>
2025-10-01 09:51:40 +0700

    обнова

--- src/components/Students/Student/Student.tsx
+++ src/components/Students/Student/Student.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Student.module.css';
+import styles from './Student.module.scss';
 import type StudentInterface from '@/types/StudentInterface';
 
 interface StudentProps {
