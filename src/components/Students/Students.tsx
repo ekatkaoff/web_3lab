ea9030ad569eae188504bb7cb2b9b39c78dfdcf1
ekatkaoff <ekaterinakazane@gmail.com>
2025-10-01 10:35:32 +0700

    ИТОГ Н4

--- src/components/Students/Students.tsx
+++ src/components/Students/Students.tsx
@@ -2,20 +2,24 @@
 
 import useStudents from '@/hooks/useStudents';
 import type StudentInterface from '@/types/StudentInterface';
+import Student from './Student/Student';
+
 
 const Students = (): React.ReactElement => {
-  const { students } = useStudents();
-
+  const { students, deleteStudentMutate } = useStudents();
+  const onDeleteHandler = (studentId: number) => {
+    console.log(studentId)
+    deleteStudentMutate(studentId)
+  }
   if (!students || students.length === 0) return <div>Студенты не найдены</div>;
 
   return (
     <div>
       <h1>Список студентов</h1>
       {students.map((student: StudentInterface) => (
-        <div key={student.id}>
-          <h3>{student.firstName} {student.lastName}</h3>
-          <p>Группа: {student.groupId}</p>
-        </div>
+        <Student student={student}
+      onDelete={onDeleteHandler}
+/>
       ))}
     </div>
   );
